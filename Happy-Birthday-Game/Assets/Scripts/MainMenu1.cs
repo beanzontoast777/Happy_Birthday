5be75c2bc6455817b9ef629e6ca41735643c5008
Bontle <yambiletlhapane@gmail.com>
2025-09-20 17:54:07 +0200

    Start Menu added
    Start Menu works now

---
+++ Happy-Birthday-Game/Assets/Scripts/MainMenu1.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu1 : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); 
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("Quit game"); 
+    }
+
+
+
+
+
+
+
+
+}
